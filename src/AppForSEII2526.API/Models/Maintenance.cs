258cabcdb09dea2e1eabaf089a3a56e4966ff73e
ALEJANDRO PATIÑO GALINDO <Alejandro.Patino@alu.uclm.es>
2025-10-01 19:50:05 +0200

    Crear clase Maintenance y sus atributos

---
+++ src/AppForSEII2526.API/Models/Maintenance.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class Maintenance
+{
+    // Propiedades
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int NumberOfDays { get; set; }
+    public decimal Price { get; set; }
+
+    // Constructor por defecto
+    public Maintenance()
+    {
+    }
+
+    // Constructor con parámetros
+    public Maintenance(int id, string name, int numberOfDays, decimal price)
+    {
+        Id = id;
+        Name = name;
+        NumberOfDays = numberOfDays;
+        Price = price;
+    }
+
+    // Método Equals
+    public override bool Equals(object obj)
+    {
+        if (obj is Maintenance other)
+        {
+            return Id == other.Id;
+        }
+        return false;
+    }
+
+    // Método GetHashCode
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
