eac5430e9779f8e9edcfda9086b04777c869a396
ALEJANDRO PATIÑO GALINDO <Alejandro.Patino@alu.uclm.es>
2025-10-01 19:53:29 +0200

    Crear clase MaintenanceType y sus atributos

---
+++ src/AppForSEII2526.API/Models/MaintenanceType.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class MaintenanceType
+{
+    // Propiedades
+    public int id { get; set; }
+    public string Type { get; set; }
+
+    // Constructor por defecto
+    public MaintenanceType()
+    {
+    }
+
+    // Constructor con parámetros
+    public MaintenanceType(int id, string type)
+    {
+        this.id = id;
+        Type = type;
+    }
+
+    // Método Equals
+    public override bool Equals(object obj)
+    {
+        if (obj is MaintenanceType other)
+        {
+            return this.id == other.id;
+        }
+        return false;
+    }
+
+    // Método GetHashCode
+    public override int GetHashCode()
+    {
+        return id.GetHashCode();
+    }
+}
