1e452b716bc7f003cfa969f99ec20a50b60ce9a8
ÁNGEL TORRIJOS ARENAS <Angel.Torrijos1@alu.uclm.es>
2025-10-02 17:00:50 +0200

    Cambios en la clase Review.cs

--- src/AppForSEII2526.API/Models/Review.cs
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -2,20 +2,16 @@ using System;
 
 public class Review
 {
-    public string Country { get; set; }
     public DateTime Created { get; set; }
-    public string DriverType { get; set; }
     public int Id { get; set; }
 
     public Review()
     {
     }
 
-    public Review(int id, string driverType, string country, DateTime created)
+    public Review(int id, DateTime created)
     {
         Id = id;
-        DriverType = driverType;
-        Country = country;
         Created = created;
     }
 
