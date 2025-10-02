3e517514c59a8a9b5117e165e60d3978b407cb8c
ALEJANDRO PATIÑO GALINDO <Alejandro.Patino@alu.uclm.es>
2025-10-02 17:43:50 +0200

    Crear clase Booking y sus atributos

--- src/AppForSEII2526.API/Models/Booking.cs
+++ src/AppForSEII2526.API/Models/Booking.cs
@@ -3,13 +3,8 @@ using System;
 public class Booking
 {
     // Propiedades
-    public string clientAddress { get; set; }
-    public string clientName { get; set; }
-    public string clientPhoneNum { get; set; }
-    public string clientSurname { get; set; }
     public DateTime Date { get; set; }
     public int Id { get; set; }
-    public string PaymentMethod { get; set; }
 
     // Constructor por defecto
     public Booking()
@@ -17,20 +12,13 @@ public class Booking
     }
 
     // Constructor con parámetros
-    public Booking(int id, string clientName, string clientSurname, string clientAddress,
-                   string clientPhoneNum, DateTime date, string paymentMethod)
+    public Booking(int id, DateTime date)
     {
         Id = id;
-        this.clientName = clientName;
-        this.clientSurname = clientSurname;
-        this.clientAddress = clientAddress;
-        this.clientPhoneNum = clientPhoneNum;
         Date = date;
-        PaymentMethod = paymentMethod;
     }
 
     // Método Equals
-    
     public override bool Equals(object obj)
     {
         if (obj is Booking otherBooking)
