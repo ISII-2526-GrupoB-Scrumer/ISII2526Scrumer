9cdc7d66af1804b5e98829c2f4b2d8ff90b7ccf1
Alvaro054 <alvaro.naranjo@alu.uclm.es>
2025-10-03 12:55:39 +0200

    Creación de la clase RentalItem.cs y sus atrubutos (#34)

---
+++ src/AppForSEII2526.API/Models/RentalItem.cs
@@ -0,0 +1,29 @@
+public class RentalItem
+{
+
+    //Atributos
+    public int CarId { get; set; }
+    public int Quantity { get; set; }
+    public int RentalId { get; set; }
+
+
+    //Métodos
+    punlic RentalItem() { }
+
+    public RentalItem(int carId, int quantity, int rentalId)
+    {
+        CarId = carId;
+        Quantity = quantity;
+        RentalId = rentalId;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
