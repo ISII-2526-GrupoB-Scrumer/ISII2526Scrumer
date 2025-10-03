31dd93474959c2cf83c0d075d64e9377f0eb8c92
Alexpati05 <alejandro.patino@alu.uclm.es>
2025-10-03 12:43:37 +0200

    Crear clase Booking y sus atributos (#24)
    * Crear clase Booking y sus atributos

---
+++ src/AppForSEII2526.API/Models/Booking.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class Booking
+{
+    // Propiedades
+    public DateTime Date { get; set; }
+    public int Id { get; set; }
+
+    // Constructor por defecto
+    public Booking()
+    {
+    }
+
+    // Constructor con parámetros
+    public Booking(int id, DateTime date)
+    {
+        Id = id;
+        Date = date;
+    }
+
+    // Método Equals
+    public override bool Equals(object obj)
+    {
+        if (obj is Booking otherBooking)
+        {
+            return this.Id == otherBooking.Id;
+        }
+        return false;
+    }
+
+    // Método GetHashCode
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
