beff1d66e46c47bef8db7723626da8d8126531d3
Alvaro054 <alvaro.naranjo@alu.uclm.es>
2025-10-03 12:55:21 +0200

    Creacion de la clase Rental.cs y sus atributos (#35)
    * Creacion de la clase Rental.cs y sus atributos
    
    * Modificación clase Rental.cs

---
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -0,0 +1,38 @@
+public class Rental
+{
+
+    //Atributos
+    public string DeliveryCarDealer { get; set; }
+    public DateTime EndDate { get; set; }
+    public int Id { get; set; }
+    public DateTime RentingDate { get; set; }
+    public DateTime StartDate { get; set; }
+    public decimal TotalPrice { get; set; }
+
+
+    //Metodos
+
+    public Rental() { }
+
+    public Rental(string deliveryCarDealer, DateTime endDate, int id, DateTime rentingDate, DateTime startDate, decimal totalPrice)
+    {
+        DeliveryCarDealer = deliveryCarDealer;
+        EndDate = endDate;
+        Id = id;
+        RentingDate = rentingDate;
+        StartDate = startDate;
+        TotalPrice = totalPrice;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+
+    }
+
+}
