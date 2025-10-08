8509a1f3fb2446b369e65654a62dbc6c5b393350
ÁLVARO NARANJO GARCÍA <Alvaro.Naranjo@alu.uclm.es>
2025-10-08 09:12:07 +0200

    Advertencia en clase PurchaseItem

--- src/AppForSEII2526.API/Models/PurchaseItem.cs
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -26,10 +26,10 @@ namespace AppForSEII2526.API.Models
         [Range(1,int.MaxValue)]
         public int Quantity { get; set; }
 
-        
+        [ForeignKey(nameof(PurchaseId))]
         public Purchase Purchase { get; set; }
 
-        
+        [ForeignKey(nameof(CarId))]
         public Car Car { get; set; }
 
         public PurchaseItem()
