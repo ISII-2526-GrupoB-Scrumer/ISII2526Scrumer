3a66f7b1ad4f92700208b36ac3be95a6e72a3249
Alvaro054 <alvaro.naranjo@alu.uclm.es>
2025-10-29 11:07:08 +0100

    Revert "Creación controladores con gets (#85)" (#95)
    This reverts commit 618207f371689954a1154f944900ae8646854a7d.

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,10 +5,4 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
-
-    public DbSet<Car> Car { get; set; } 
-    public DbSet<Maintenance> Maintenance { get; set; }
-
-
-
 }
