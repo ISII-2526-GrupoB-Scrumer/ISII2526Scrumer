3d37feceaca089d72ee9134c9de21c46e594e08c
ÁLVARO NARANJO GARCÍA <Alvaro.Naranjo@alu.uclm.es>
2025-10-31 11:44:52 +0100

    correcion

--- src/AppForSEII2526.API/Controllers/MaintenanceController.cs
+++ src/AppForSEII2526.API/Controllers/MaintenanceController.cs
@@ -20,31 +20,6 @@ namespace AppForSEII2526.API.Controllers
         }
 
 
-        [HttpGet]
-        [Route("[action]")]
-        [ProducesResponseType(typeof(IList<MaintenanceSelectDTO>), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult> GetMaintenances(string? nombre, string? tipo)
-        {
-            IList<MaintenanceSelectDTO> mantenimientos = await _context.Maintenance
-                .Include(m => m.MaintenanceTypes)
-                .Where(m =>
-                    (nombre == null || m.Name.Contains(nombre)) &&
-                    (tipo == null || m.MaintenanceTypes.Any(t => t.Type.Contains(tipo)))
-                )
-                .OrderBy(m => m.Name)
-                .ThenBy(m => m.Price)
-                .Select(m => new MaintenanceSelectDTO(
-                    m.Id,
-                    m.Name,
-                    m.MaintenanceTypes.FirstOrDefault() != null
-                        ? m.MaintenanceTypes.FirstOrDefault()!.Type
-                        : "Sin tipo",
-                    m.Price,
-                    m.NumberOfDays
-                ))
-                .ToListAsync();
-
-            return Ok(mantenimientos);
-        }
+       
     }
 }
