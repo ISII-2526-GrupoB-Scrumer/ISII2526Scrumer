617b922bc09ae6857cd936973eb5745250b806de
Angel3546 <angel.torrijos1@alu.uclm.es>
2025-12-04 10:51:36 +0100

    301 implementación interfaz select cu review (#140)
    * Conexión con la API (#136) (#138)
    
    * Implementación del Select de la Web
    
    * borrado de zip
    
    ---------
    
    Co-authored-by: Alvaro054 <alvaro.naranjo@alu.uclm.es>

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -43,6 +43,7 @@ string? URI2API = builder.Configuration.GetValue(typeof(string), "AppForSEII2526
 //We create the service for accessing the API from where .WEB project
 builder.Services.AddScoped<AppForSEII2526APIClient>(sp => new AppForSEII2526APIClient(URI2API, new HttpClient()));
 
+builder.Services.AddScoped<ReviewStateContainer>();
 
 var app = builder.Build();
 
---
+++ src/AppForSEII2526.Web/ReviewStateContainer.cs
@@ -0,0 +1,64 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class ReviewStateContainer
+    {
+        // Instancia de Review cuando se crea el ReviewStateContainer
+        public ReviewCreateDTO Review { get; private set; } = new ReviewCreateDTO()
+        {
+            ReviewItems = new List<ReviewItemDTO>()
+        };
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        // Añadir un coche a la review
+        public void AddCarToReview(ReviewSelectDTO car)
+        {
+            // evitar duplicados
+            if (!Review.ReviewItems.Any(ri => ri.CarId == car.Id))
+            {
+                Review.ReviewItems.Add(new ReviewItemDTO()
+                {
+                    CarId = car.Id,
+                    CarModel = car.ModelName,
+                    Manufacturer = car.Manufacturer,
+                    FuelType = car.FuelType,
+                    Color = car.Color,
+                    Description = string.Empty,  // lo rellenará el usuario
+                    Rating = 0                   // lo rellenará el usuario
+                });
+            }
+
+            NotifyStateChanged();
+        }
+
+
+        // Eliminar un ítem de review
+        public void RemoveReviewItem(ReviewItemDTO item)
+        {
+            Review.ReviewItems.Remove(item);
+            NotifyStateChanged();
+        }
+
+        // Vaciar todos los ítems de la review
+        public void ClearReview()
+        {
+            Review.ReviewItems.Clear();
+            NotifyStateChanged();
+        }
+
+        // Cuando ya hemos procesado la review, reiniciamos el objeto
+        public void ReviewProcessed()
+        {
+            Review = new ReviewCreateDTO()
+            {
+                ReviewItems = new List<ReviewItemDTO>()
+            };
+
+            NotifyStateChanged();
+        }
+    }
+}
