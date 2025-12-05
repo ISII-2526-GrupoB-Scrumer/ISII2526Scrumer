0c00cb1769c9ec7445aa6cd531a47201cbeb3260
ÁLVARO NARANJO GARCÍA <Alvaro.Naranjo@alu.uclm.es>
2025-12-05 11:56:09 +0100

    Pruebas funcionales Rental

---
+++ test/AppForSEII2526.UIT/UC-Rental/CURental_UIT.cs
@@ -0,0 +1,16 @@
+﻿using AppForSEII2526.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Rental
+{
+    public class CURental_UIT : UC_UIT
+    {
+        public CURental_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/UC-Rental/SelectCarsforRentalPO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Rental
+{
+    internal class SelectCarsforRentalPO : PageObject
+    {
+        protected SelectCarsforRentalPO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+    }
+}
