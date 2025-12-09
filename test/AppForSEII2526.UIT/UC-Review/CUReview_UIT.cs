5064236bbff2d7ef0d749a138b781e15d3045e84
ÁNGEL TORRIJOS ARENAS <Angel.Torrijos1@alu.uclm.es>
2025-12-09 10:22:07 +0100

    Creación de las pruebas funcionales para el CU Review

---
+++ test/AppForSEII2526.UIT/UC-Review/CUReview_UIT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Review
+{
+    internal class CUReview_UIT : UC_UIT
+    {
+        public CUReview_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
