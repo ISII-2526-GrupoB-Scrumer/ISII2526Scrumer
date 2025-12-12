930640764e904613a85842b5f166fb04244f3b97
ÁLVARO NARANJO GARCÍA <Alvaro.Naranjo@alu.uclm.es>
2025-12-12 12:37:29 +0100

    implementacion pruebas

--- test/AppForSEII2526.UIT/UC-Rental/SelectCarsforRentalPO.cs
+++ test/AppForSEII2526.UIT/UC-Rental/SelectCarsforRentalPO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,45 @@ using System.Threading.Tasks;
 
 namespace AppForSEII2526.UIT.UC_Rental
 {
+
     internal class SelectCarsforRentalPO : PageObject
     {
+
+        private By inputModelo = By.Id("inputModelo");
+        private By inputPrecio = By.Id("inputPrecio");
+        private By buttonBuscar = By.Id("buttonBuscar");
+        
         protected SelectCarsforRentalPO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
         }
+
+        public void BuscarCoches(string modelo,int precio)
+        {
+            WaitForBeingClickable(inputModelo);
+            _driver.FindElement(inputModelo).Clear();
+            _driver.FindElement(inputModelo).Click();
+            _driver.FindElement(inputModelo).SendKeys(modelo);
+            _driver.FindElement(buttonBuscar).Click();
+        }
+
+        public void AddCarToCart(string modelo)
+        {
+            WaitForBeingClickable(By.Id("carToRent_" + modelo));
+
+            _driver.FindElement(By.Id("carToRent_" + modelo)).Click();
+        }
+
+        public void RemoveCarFromCart(string modelo)
+        {
+            WaitForBeingClickable(By.Id("removeCar_" + modelo));
+            _driver.FindElement(By.Id("removeCar_" + modelo)).Click();
+        }
+
+        public bool RentingNotAvailable()
+        {
+
+            return _driver.FindElement(buttonBuscar).Displayed == false;
+        }
+
     }
 }
