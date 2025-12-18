dc5f7b156f32f78f691451f2fccdba1f70edb2c2
Alvaro054 <alvaro.naranjo@alu.uclm.es>
2025-12-18 10:50:25 +0100

    Cambios examen CU Rental (#192)
    Co-authored-by: Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>

--- test/AppForSEII2526.UIT/UC-Rental/CURentalCars_UIT.cs
+++ test/AppForSEII2526.UIT/UC-Rental/CURentalCars_UIT.cs
@@ -27,6 +27,7 @@ namespace AppForSEII2526
         private const int carId2 = 6;
         private const string carmodel2 = "Civic";
         private const int carprice2 = 120;
+        private const int totalprice2 = (120 * 7);
 
 
         public CURentalCars_UIT(ITestOutputHelper output) : base(output)
@@ -221,6 +222,50 @@ namespace AppForSEII2526
             //Assert
             Assert.True(selectcarsForRental_PO.CheckBodyTable(expected, By.Id("TableOfRentalItems")));
 
+
+        }
+
+        [Theory]
+        [InlineData("Carlos", "Lopez", "U1", "Calle Sol 123, Madrid", "Visa")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC2_Examen(string nombre, string apellido, string id, string direccion, string pago) //Rental correcto 
+        {
+            //Arrange
+            Initial_step_opening_the_web_page();
+            _driver.Navigate().GoToUrl(_URI + "rental/selectcarsforrental");
+            Thread.Sleep(2000);
+            selectcarsForRental_PO = new SelectCarsForRentalPO(_driver, _output);
+            createRental_PO = new CreateRentalPO(_driver, _output);
+            detailRental_PO = new DetailRentalPO(_driver, _output);
+
+            //Act
+            selectcarsForRental_PO.SearchCars("Civic", ""); //filtro primer coche (honda)
+            Thread.Sleep(1000);
+            selectcarsForRental_PO.AddCarToCart(carId2); //añado primer coche
+            Thread.Sleep(1000);
+            selectcarsForRental_PO.SearchCars("", "75"); //filtro segundo coche (corrola)
+            Thread.Sleep(1000);
+            selectcarsForRental_PO.AddCarToCart(carId1); //añado segundo coche
+            Thread.Sleep(1000);
+            selectcarsForRental_PO.ConfirmRental();
+            Thread.Sleep(1000);
+            createRental_PO.ModifyCars(); //voy a create
+            Thread.Sleep(1000);
+            selectcarsForRental_PO.RemoveCarFromCart(carId2); // quito primer coche (honda)
+            Thread.Sleep(1000);
+
+            selectcarsForRental_PO.ConfirmRental(); //vuelvo a create
+            Thread.Sleep(1000);
+
+            createRental_PO.FillRentalForm(nombre, apellido, id, direccion, pago);
+            Thread.Sleep(1000);
+            createRental_PO.ConfirmRental();
+            Thread.Sleep(1000);
+            createRental_PO.ConfirmDialog();
+
+            //Assert
+            Thread.Sleep(1000);
+            Assert.True(detailRental_PO.CheckRentalDetail($"{nombre} {apellido}", direccion, pago, totalprice1));
 
         }
     }
