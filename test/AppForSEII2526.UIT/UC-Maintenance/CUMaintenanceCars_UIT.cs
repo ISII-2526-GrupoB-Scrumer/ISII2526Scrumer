690a5de4e7a25d0e1a2ae8d322489fd2da337f3e
Alexpati05 <alejandro.patino@alu.uclm.es>
2026-02-25 18:33:37 +0100

    400 correcciones cu maintenance (#198)
    * Cambios en las pruebas de maintenance
    
    * Archivo

--- test/AppForSEII2526.UIT/UC-Maintenance/CUMaintenanceCars_UIT.cs
+++ test/AppForSEII2526.UIT/UC-Maintenance/CUMaintenanceCars_UIT.cs
@@ -25,12 +25,12 @@ namespace AppForSEII2526.UIT.UC_Maintenance
 
         [Theory]
         [Trait("LevelTesting", "Functional")]
-        [InlineData("carlitos_l", "Calle Sol 123, Madrid", "600000001", "Visa", "Comentario test")]
-        [InlineData("carlitos_l", "Calle Sol 123, Madrid", "600000001", "Paypal", "Comentario test")]
-        [InlineData("carlitos_l", "Calle Sol 123, Madrid", "600000001", "Google Pay", "Comentario test")]
-        public void UC_Maintenance_FullFlow_Success(string nom, string dir, string tel, string pago, string com)
+        [InlineData("carlitos_l", "Calle Sol 123, Madrid", "600000001", "Visa", "Comentario test", "100")]
+        [InlineData("carlitos_l", "Calle Sol 123, Madrid", "600000001", "Paypal", "Comentario test", "100")]
+        [InlineData("carlitos_l", "Calle Sol 123, Madrid", "600000001", "Google Pay", "Comentario test", "100")]
+        public void UC3_1_2_3(string nom, string dir, string tel, string pago, string com, string precioEsperado)
         {
-            // Arrange
+            // 1. Arrange
             Initial_step_opening_the_web_page();
             _driver.Navigate().GoToUrl(_URI + "maintenance/selectcarsformaintenance");
 
@@ -38,20 +38,21 @@ namespace AppForSEII2526.UIT.UC_Maintenance
             createPO = new CreateMaintenancePO(_driver, _output);
             detailPO = new DetailMaintenancePO(_driver, _output);
 
-            // Act
-            selectPO.AddToCart(1); 
+            // 2. Act
+            selectPO.AddToCart(1);
             selectPO.ContinueToBooking();
 
             createPO.FillForm(nom, dir, tel, pago, com);
             createPO.ClickContratar();
 
-            // Assert
-            Assert.True(detailPO.CheckDetails(nom, dir, ""));
+            // 3. Assert
+            Assert.True(detailPO.CheckDetails(nom, dir, precioEsperado), "Los datos generales o el precio no coinciden.");
+            Assert.True(_driver.PageSource.Contains("Cambio aceite"), "El servicio 'Cambio aceite' no aparece en el detalle.");
         }
 
         [Fact]
         [Trait("LevelTesting", "Functional Testing")]
-        public void UC3_4() 
+        public void UC3_4()
         {
             Initial_step_opening_the_web_page();
             _driver.Navigate().GoToUrl(_URI + "maintenance/selectcarsformaintenance");
@@ -59,7 +60,7 @@ namespace AppForSEII2526.UIT.UC_Maintenance
 
             selectPO.SearchMaintenance("xxxxx", "xxxxx");
 
-            var expected = new List<string[]>(); 
+            var expected = new List<string[]>();
             Assert.True(selectPO.CheckBodyTable(expected, By.Id("TableOfMaintenances")));
         }
 
@@ -68,7 +69,7 @@ namespace AppForSEII2526.UIT.UC_Maintenance
         [InlineData("", "Motor")]
         [InlineData("Aceite", "Motor")]
         [Trait("LevelTesting", "Functional Testing")]
-        public void UC3_5_6_7(string nombre, string tipo) 
+        public void UC3_5_6_7(string nombre, string tipo)
         {
             Initial_step_opening_the_web_page();
             _driver.Navigate().GoToUrl(_URI + "maintenance/selectcarsformaintenance");
@@ -79,7 +80,7 @@ namespace AppForSEII2526.UIT.UC_Maintenance
 
             var expected = new List<string[]>
             {
-                
+
                 new[] { maintName1, maintType1 }
             };
 
@@ -88,7 +89,7 @@ namespace AppForSEII2526.UIT.UC_Maintenance
 
         [Fact]
         [Trait("LevelTesting", "Functional Testing")]
-        public void UC3_8() 
+        public void UC3_8()
         {
             Initial_step_opening_the_web_page();
             _driver.Navigate().GoToUrl(_URI + "maintenance/selectcarsformaintenance");
@@ -101,13 +102,13 @@ namespace AppForSEII2526.UIT.UC_Maintenance
         }
 
         [Theory]
-        [InlineData("", "Calle Sol 123, Madrid", "600000001", "Visa", "Comentario")] 
-        [InlineData("carlitos_l", "", "600000001", "Visa", "Comentario")] 
-        [InlineData("carlitos_l", "Calle Sol 123, Madrid", "600000001", "", "Comentario")]       
+        [InlineData("", "Calle Sol 123, Madrid", "600000001", "Visa", "Comentario")]
+        [InlineData("carlitos_l", "", "600000001", "Visa", "Comentario")]
+        [InlineData("carlitos_l", "Calle Sol 123, Madrid", "600000001", "", "Comentario")]
         [InlineData("carlitos_l", "Calle Sol 123, Madrid", "600000001", "Visa", "")]
         [Trait("LevelTesting", "Functional Testing")]
 
-        public void UC3_10_11_12_13_14(string nom, string dir, string tel, string pago, string com) 
+        public void UC3_9_10_11_12(string nom, string dir, string tel, string pago, string com)
         {
             Initial_step_opening_the_web_page();
             _driver.Navigate().GoToUrl(_URI + "maintenance/selectcarsformaintenance");
@@ -120,13 +121,13 @@ namespace AppForSEII2526.UIT.UC_Maintenance
             createPO.FillForm(nom, dir, tel, pago, com);
             createPO.ClickContratar();
 
-            Assert.True(createPO.ErrorVisible()); 
+            Assert.True(createPO.ErrorVisible());
         }
 
         [Fact]
         [Trait("LevelTesting", "Functional Testing")]
 
-        public void UC3_15() 
+        public void UC3_13()
         {
             Initial_step_opening_the_web_page();
             _driver.Navigate().GoToUrl(_URI + "maintenance/selectcarsformaintenance");
@@ -148,8 +149,8 @@ namespace AppForSEII2526.UIT.UC_Maintenance
 
         [Theory]
         [Trait("LevelTesting", "Functional Testing")]
-        [InlineData("carlitos_l", "Calle Sol 123, Madrid", "600000001", "Visa", "comentario")]
-        public void UC3_16(string nom, string dir, string tel, string pago, string com)
+        [InlineData("carlitos_l", "Calle Sol 123, Madrid", "600000001", "Visa", "comentario","400")]
+        public void UC3_14(string nom, string dir, string tel, string pago, string com,string precioEsperado)
         {
             // 1. Arrange: Abrir página y preparar POs
             Initial_step_opening_the_web_page();
@@ -183,7 +184,8 @@ namespace AppForSEII2526.UIT.UC_Maintenance
             Thread.Sleep(1000);
 
             // 7. Assert: Verificar en el detalle que aparecen los datos y el precio total
-            Assert.True(detailPO.CheckDetails(nom, dir, ""));
+            Assert.True(detailPO.CheckDetails(nom, dir, precioEsperado), "Los datos generales o el precio no coinciden.");
+            Assert.True(_driver.PageSource.Contains("Cambio aceite"), "El servicio 'Cambio aceite' no aparece en el detalle.");
         }
     }
 }
